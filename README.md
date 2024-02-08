b0ae3f46f6923568c9adb72a7ec9a76ac74ca168
Soo Hwan Na <whiteshell2544@naver.com>
2024-02-08 09:19:28 +0900

    warsaw: Initial TWRP device tree
    Made with SebaUbuntu's TWRP device tree generator
    Arch: arm64
    Manufacturer: Huawei
    Model: warsaw
    Script version: 3.0.0

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for Huawei P11 Lite (warsaw)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ omni_warsaw.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_DEVICE := warsaw
+PRODUCT_NAME := omni_warsaw
+PRODUCT_MANUFACTURER := huawei
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="generic_a15-user 8.0.0 OPR6.170623.010 eng.jenkin.20190326.143923 test-keys"
+
+BUILD_FINGERPRINT := Huawei/generic_a15/generic_a15:8.0.0/OPR6.170623.010/jenkin03261439:user/test-keys
